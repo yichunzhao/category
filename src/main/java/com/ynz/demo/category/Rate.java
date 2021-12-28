051717792814f82443dcff50c7b88983fc3458dd
yichunzhao <zhaoyichun@gmail.com>
2021-12-28 19:03:34 +0100

    java >=12, Collector teeing, operating both collectors and merging results.

--- src/main/java/com/ynz/demo/category/Rate.java
+++ src/main/java/com/ynz/demo/category/Rate.java
@@ -8,8 +8,13 @@ import java.util.UUID;
 
 @Data
 @AllArgsConstructor
-public class Rate {
+public class Rate implements Comparable<Rate>{
   private UUID clientId;
   private YearMonth date;
   private int score;
+
+  @Override
+  public int compareTo(Rate o) {
+    return Integer.compare(score, o.getScore());
+  }
 }
--- src/main/java/com/ynz/demo/category/RatingCategory.java
+++ src/main/java/com/ynz/demo/category/RatingCategory.java
@@ -2,15 +2,14 @@ package com.ynz.demo.category;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class RatingCategory {
 
   public static void main(String[] args) {
-
     var rateList = TestData.instance().genTestRates();
-
     var scoreRateListMap = rateList.stream().collect(Collectors.groupingBy(Rate::getScore));
     log.info("Score RateList Map: {}", scoreRateListMap);
 
@@ -19,7 +18,20 @@ public class RatingCategory {
     log.info("score greaterThanAndEqualTo 4, RateList: {}", scoreGreaterEqualToScoreFour.get(true));
     log.info("score lessThan 4, RateList: {}", scoreGreaterEqualToScoreFour.get(false));
 
-    //collector to unModified set
+    var min = rateList.stream().min(Rate::compareTo);
+    log.info("Rate having min score: {} ", min.isPresent() ? min.get() : "not present!");
 
+    var max = rateList.stream().max(Rate::compareTo);
+    log.info("Rate having max score: {} ", max.isPresent() ? max.get() : "not present!");
+
+    // teeing both Collectors in one statement,ana merging both results together.
+    var teeingBoth =
+        rateList.stream()
+            .collect(
+                Collectors.teeing(
+                    Collectors.maxBy(Rate::compareTo),
+                    Collectors.minBy(Rate::compareTo),
+                    List::of));
+    log.info("teeing both max and min: {}", teeingBoth);
   }
 }
